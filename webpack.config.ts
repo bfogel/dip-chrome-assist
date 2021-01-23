8d65ddd91103046905080e71b250d5081743684a
Thomas Anderson <tommy@Thomass-MacBook-Pro.local>
2021-01-23 15:28:41 -0500

    Add typescript + linting and remove background

---
+++ webpack.config.ts
@@ -0,0 +1,44 @@
+import * as webpack from 'webpack';
+
+import CopyPlugin from 'copy-webpack-plugin';
+import ExtensionReloader from 'webpack-extension-reloader';
+
+const config: webpack.Configuration = {
+    entry: {
+        'content': './src/content.ts'
+    },
+
+    output: {
+        path: `${__dirname}/dist/`,
+        publicPath: '/dist/',
+        filename: './[name].js'
+    },
+
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin([
+            { from: 'manifest.json' }
+        ]),
+        new (ExtensionReloader as any)({
+            port: 9090,
+            reloadPage: true,
+            entries: {
+                contentScript: ['content']
+            }
+        })
+    ]
+};
+
+export default config;
