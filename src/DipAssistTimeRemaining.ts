819dbce3749a8833be7226c949f6b5e0ebedb48e
StevenHogue <stevenbhogue@gmail.com>
2021-01-24 18:45:52 -0500

    add english/french language option to DipAssistTimeRemaining constructor

--- src/DipAssistTimeRemaining.ts
+++ src/DipAssistTimeRemaining.ts
@@ -1,14 +1,18 @@
 class DipAssistTimeRemaining{
-  public days: number;
-  public hours: number;
-  public minutes: number;
-  public seconds: number;
-  public timeRemainingDisplay: HTMLElement | null;
+  public readonly days: number;
+  public readonly hours: number;
+  public readonly minutes: number;
+  public readonly seconds: number;
+  public readonly timeRemainingDisplay: HTMLElement | null;
+  public readonly language: string;
 
-  public constructor(milliseconds : number)
+  public constructor(milliseconds : number, language : string = "English")
   {
     this.milliseconds = milliseconds;
-    this.InitializeFromMilliseconds();
+    this.days = this.DaysFromMilliseconds();
+    this.hours = this.HoursFromMilliseconds();
+    this.minutes = this.MinutesFromMilliseconds();
+    this.seconds = this.SecondsFromMilliseconds();
   }
 
   public GetSpanStylePartFromTimeRemaining() : string
@@ -20,35 +24,35 @@ class DipAssistTimeRemaining{
 
   public GetTimeRemainingDisplayValue() : string 
   {
-      var daysPart = this.days + " days, ";
-      var hoursPart = this.hours + " hours, ";
-      var minutesPart = this.minutes + " minutes, ";
-      var secondsPart = this.seconds + " seconds";
+      var daysPart = this.days + " " + this.GetDaysDisplayValueByLanguage() + ", ";
+      var hoursPart = this.hours + " " + this.GetHoursDisplayValueByLanguage() + ", ";
+      var minutesPart = this.minutes + " " + this.GetMinutesDisplayValueByLanguage() + ", ";
+      var secondsPart = this.seconds + " " + this.GetSecondsDisplayValueByLanguage();
 
-      var report;
+      var displayValue;
 
       if (this.days > 0)
       {
-        report = daysPart + hoursPart + minutesPart + secondsPart;
+        displayValue = daysPart + hoursPart + minutesPart + secondsPart;
       }
       else if (this.hours > 0)
       {
-        report = hoursPart + minutesPart + secondsPart;
+        displayValue = hoursPart + minutesPart + secondsPart;
       }
       else if (this.minutes > 0)
       {
-        report = minutesPart + secondsPart;
+        displayValue = minutesPart + secondsPart;
       }
       else if (this.seconds > 0)
       {
-        report = secondsPart;
+        displayValue = secondsPart;
       }
       else
       {
-        report = "DEADLINE PASSED!";
+        displayValue = "The deadline has passed.";
       }
 
-      return report;
+      return displayValue;
   }
 
   private DaysFromMilliseconds() : number
@@ -88,17 +92,77 @@ class DipAssistTimeRemaining{
     return Math.floor(millisecondsAfterMinutes / this.MILLISECONDS_PER_SECOND);
   }
 
+  private GetDaysDisplayValueByLanguage()
+  {
+    let displayValue : string;
+    switch(this.language)
+    {
+      case "English":
+          displayValue = this.hours == 1 || this.hours == -1 ? "day" : "days";
+        break;
+      case "French":
+        displayValue = this.hours == 1 || this.hours == -1 ? "journée" : "journées";
+        break;
+      default:
+          throw new Error("unsupported language in DipAssistTimeRemaining: " + this.language);
+    }
+    return displayValue;
+  }
+
+  private GetHoursDisplayValueByLanguage()
+  {
+    let displayValue : string;
+    switch(this.language)
+    {
+      case "English":
+        displayValue = this.hours == 1 || this.hours == -1 ? "hour" : "hours";
+        break;
+      case "French":
+        displayValue = this.hours == 1 || this.hours == -1 ? "heure" : "heures";
+        break;
+      default:
+          throw new Error("unsupported language in DipAssistTimeRemaining: " + this.language);
+    }
+    return displayValue;
+  }
+
+  private GetMinutesDisplayValueByLanguage()
+  {
+    let displayValue : string;
+    switch(this.language)
+    {
+      case "English":
+        displayValue = this.minutes == 1 || this.minutes == -1 ? "minute" : "minutes";
+        break;
+      case "French":
+        displayValue = this.minutes == 1 || this.minutes == -1 ? "minute" : "minutes";
+        break;
+      default:
+          throw new Error("unsupported language in DipAssistTimeRemaining: " + this.language);
+    }
+    return displayValue;
+  }
+
+  private GetSecondsDisplayValueByLanguage()
+  {
+    let displayValue : string;
+    switch(this.language)
+    {
+      case "English":
+        displayValue = this.seconds == 1 || this.seconds == -1 ? "second" : "seconds";
+        break;
+      case "French":
+        displayValue = this.seconds == 1 || this.seconds == -1 ? "seconde" : "secondes";
+        break;
+      default:
+          throw new Error("unsupported language in DipAssistTimeRemaining: " + this.language);
+    }
+    return displayValue;
+  }
+
   private milliseconds: number;
   private MILLISECONDS_PER_DAY : number = 1000 * 60 * 60 * 24;
   private MILLISECONDS_PER_HOUR : number = 1000 * 60 * 60;
   private MILLISECONDS_PER_MINUTE : number = 1000 * 60;
   private MILLISECONDS_PER_SECOND : number = 1000;
-  
-  private InitializeFromMilliseconds()
-  {
-    this.days = this.DaysFromMilliseconds();
-    this.hours = this.HoursFromMilliseconds();
-    this.minutes = this.MinutesFromMilliseconds();
-    this.seconds = this.SecondsFromMilliseconds();
-  }
 }
